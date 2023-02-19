df1b4d18a93b3b6334f5e805a43532f79dedd9a2
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 16:59:18 -0300

    Update Exercicio 9.c

--- Exercicio 9.c
+++ Exercicio 9.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+//Exercício 9: Monte um programa que exiba acentuação em C utilizando a lib <locale.h>, dica: utilize o setlocale.
+
 main(void){
 	setlocale(LC_ALL, "portuguese");
 	printf("arajá");
