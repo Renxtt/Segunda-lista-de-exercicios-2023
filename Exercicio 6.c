77330fd8720b2764bfcd38815b9ad3c34363421c
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 16:52:51 -0300

    Update Exercicio 6.c

--- Exercicio 6.c
+++ Exercicio 6.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <math.h>
-
 #include <stdio.h>
+
+//Exercício 6: Ler um número do usuário e calcular a raiz quadrada desse número.
 
 main(void){
 	int raiz, nub, resu;
